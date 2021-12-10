443cee4658389a636986194ea61f1806cd57bdaf
Tamyres <tamyrescaroline.oliveira@gmail.com>
2021-12-09 23:53:39 -0300

    explore food and drinks in progress

--- src/pages/BeverageExplorer.js
+++ src/pages/BeverageExplorer.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function BeverageExplorer() {
+  const history = useHistory();
+
+  const handleSurprise = async () => {
+    
+  }
   return (
     <>
       <Header name="Explorar Bebidas" />
+      <button
+        data-testid="explore-by-ingredient"
+        type="button"
+        onClick={ () => history.push('/BeverageExplorerByIngredients') }
+      >
+        Por Ingredientes
+      </button>
+      <button
+        data-testid="explore-by-area"
+        type="button"
+        onClick={ () => history.push('/BeverageExplorerByArea') }
+      >
+        Por Local de Origem
+      </button>
+      <button
+        data-testid="explore-surprise"
+        type="button"
+        onClick={ () => handleSurprise }
+      >
+        Me surpreenda!
+      </button>
       <Footer />
     </>
   );
--- src/pages/FoodExplorer.js
+++ src/pages/FoodExplorer.js
@@ -3,9 +3,24 @@ import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function FoodExplorer() {
+  const history = useHistory();
   return (
     <div>
       <Header name="Explorar Comidas" />
+      <button
+        data-testid="explore-by-ingredient"
+        type="button"
+        onClick={ () => history.push('/FoodExplorerByIngredients') }
+      >
+        Por Ingredientes
+      </button>
+      <button
+        data-testid="explore-surprise"
+        type="button"
+        onClick={ () => {} }
+      >
+        Me surpreenda!
+      </button>
       <Footer />
     </div>
   );
