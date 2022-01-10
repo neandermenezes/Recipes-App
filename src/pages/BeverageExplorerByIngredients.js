b081a0eff7fa9fa991e14eb3219fd69020f7738d
Tamyres <tamyrescaroline.oliveira@gmail.com>
2022-01-10 20:35:50 -0300

    finaliza páginas de explorar por ingredientes

--- src/pages/BeverageExplorerByIngredients.js
+++ src/pages/BeverageExplorerByIngredients.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import { requestIngredients } from '../services/fetchAPIs';
+import IngredientCard from '../components/IngredientCard';
+
+const NUMBER_OF_INGREDIENTS = 12;
 
 function BeverageExplorerByIngredients() {
+  const [ingredients, setIngredients] = useState([]);
+  useEffect(() => {
+    requestIngredients('thecocktaildb')
+      .then(({ drinks }) => drinks.map(({ strIngredient1 }) => strIngredient1))
+      .then((result) => setIngredients(result.slice(0, NUMBER_OF_INGREDIENTS)));
+  }, []);
   return (
     <>
       <Header name="Explorar Ingredientes" />
+      {ingredients.length > 0 && ingredients.map((ingredient, index) => (
+        <IngredientCard
+          key={ ingredient }
+          ingredient={ ingredient }
+          type="bebidas"
+          img={ `https://www.thecocktaildb.com/images/ingredients/${ingredient}-Small.png` }
+          index={ index }
+        />
+      ))}
       <Footer />
     </>
   );
--- src/services/fetchAPIs.js
+++ src/services/fetchAPIs.js
@@ -82,3 +82,11 @@ export async function requestRecipesById(id, urlByCategory) {
   const result = await response.json();
   return result;
 }
+
+export async function requestIngredients(urlByCategory) {
+  const response = await fetch(
+    `https://www.${urlByCategory}.com/api/json/v1/1/list.php?i=list`,
+  );
+  const result = await response.json();
+  return result;
+}
