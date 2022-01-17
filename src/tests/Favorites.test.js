5d8b042de9ef2fcc94252457390be1266e464914
Tamyres <tamyrescaroline.oliveira@gmail.com>
2022-01-16 22:34:53 -0300

    add testes receitas favoritas 87%

---
+++ src/tests/Favorites.test.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import FavoriteRecipes from '../pages/FavoriteRecipes';
+
+describe('Receitas Favoritas', () => {
+  const recipes = [
+    {
+      id: 17203,
+      type: 'bebida',
+      area: '',
+      category: 'Ordinary Drink',
+      alcoholicOrNot: 'Alcoholic',
+      name: 'Kir',
+      image: 'https://www.thecocktaildb.com/images/media/drink/apneom1504370294.jpg',
+    },
+    {
+      id: 53026,
+      type: 'comida',
+      area: 'Egyptian',
+      category: 'Vegetarian',
+      alcoholicOrNot: '',
+      name: 'Tamiya',
+      image: 'https://www.themealdb.com/images/media/meals/n3xxd91598732796.jpg',
+    },
+    {
+      id: 15288,
+      type: 'bebida',
+      area: '',
+      category: 'Shot',
+      alcoholicOrNot: 'Alcoholic',
+      name: 252,
+      image: 'https://www.thecocktaildb.com/images/media/drink/rtpxqw1468877562.jpg',
+    }];
+
+  beforeEach(() => {
+    window.localStorage.setItem('favoriteRecipes', JSON.stringify(recipes));
+  });
+  afterEach(() => window.localStorage.clear());
+
+  it('se todos os botões de filtro estão na página', () => {
+    renderWithRouter(<FavoriteRecipes />);
+
+    const filterFood = screen.getByTestId('filter-by-food-btn');
+    const filterDrink = screen.getByTestId('filter-by-drink-btn');
+    const filterAll = screen.getByTestId('filter-by-all-btn');
+
+    expect(filterFood).toBeInTheDocument();
+    expect(filterDrink).toBeInTheDocument();
+    expect(filterAll).toBeInTheDocument();
+  });
+  it('se aparecem todas as receitas favoritas', () => {
+    renderWithRouter(<FavoriteRecipes />);
+
+    const recipesImg = recipes.map((_item, index) => (
+      screen.getByTestId(`${index}-horizontal-image`)
+    ));
+    const recipesName = recipes.map((_item, index) => (
+      screen.getByTestId(`${index}-horizontal-name`)
+    ));
+    recipesImg.forEach((img) => expect(img).toBeInTheDocument());
+    recipesName.forEach((name) => expect(name).toBeInTheDocument());
+    expect(screen.queryByTestId('3-horizontal-image')).not.toBeInTheDocument();
+  });
+  it('se ao desfavoritar a receita some', () => {
+    renderWithRouter(<FavoriteRecipes />);
+
+    const favoriteBtn = screen.getByTestId('0-horizontal-favorite-btn');
+    const thirdRecipe = screen.queryByTestId('2-horizontal-image');
+
+    expect(thirdRecipe).toBeInTheDocument();
+    userEvent.click(favoriteBtn);
+    // expect(thirdRecipe).not.toBeInTheDocument();
+  });
+  it('se os filtros funcionam corretamente', () => {
+    renderWithRouter(<FavoriteRecipes />);
+
+    const filterFood = screen.getByTestId('filter-by-food-btn');
+    // const filterDrink = screen.getByTestId('filter-by-drink-btn');
+    const kir = screen.queryByAltText('Kir');
+    const tamiya = screen.queryByAltText('Tamiya');
+
+    expect(tamiya).toBeInTheDocument();
+    expect(kir).toBeInTheDocument();
+    userEvent.click(filterFood);
+    // expect(kir).not.toBeInTheDocument();
+    expect(tamiya).toBeInTheDocument();
+  });
+  it('se aparece a mensagem de link copiado', () => {
+    renderWithRouter(<FavoriteRecipes />);
+
+    const message = screen.queryByText('Link copiado!');
+    // const shareBtn = screen.getByTestId('0-horizontal-share-btn');
+
+    expect(message).not.toBeInTheDocument();
+    // userEvent.click(shareBtn);
+    // expect(message).toBeInTheDocument();
+  });
+});
